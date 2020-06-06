53c42459902866776028ab0d987158efb7df99f9
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2020-06-05 22:44:35 -0400

    Import existing code

---
+++ public/main.js
@@ -0,0 +1,22 @@
+//import * as session from './session.js';
+
+// New, play, stop buttons
+//let btnNew = document.getElementById('btn_new');
+let btnPlay = document.getElementById('btn_play');
+let btnStop = document.getElementById('btn_stop');
+
+document.body.onload = function ()
+{
+}
+
+window.onunload = function ()
+{
+}
+
+btnPlay.onclick = function ()
+{
+}
+
+btnStop.onclick = function ()
+{
+}
---
+++ public/midi.js
@@ -0,0 +1,108 @@
+let midi = null;
+
+let inputCallbacks = [];
+
+export function addInputListener(callback)
+{
+    inputCallbacks.push(callback);
+}
+
+export function removeInputListener(callback)
+{
+    inputCallbacks = inputCallbacks.filter(value => value != callback);
+}
+
+/// Send a message to all MIDI devices
+export function sendAll(msg)
+{
+    if (!midi)
+        return;
+
+    for (let output of midi.outputs.values())
+        output.send(msg);
+}
+
+function onMidiMessage(event)
+{
+    //console.log('got', event.data.length, 'bytes');
+
+    var str = '';
+    for (var i = 0; i < event.data.length; i++)
+    {
+        str += "0x" + event.data[i].toString(16) + " ";
+    }
+    console.log(str);
+
+    for (let cb of inputCallbacks)
+    {
+        cb(event.data);
+    }
+}
+
+function onMidiSuccess(midiAccess)
+{
+    console.log("MIDI ready");
+
+    midi = midiAccess;
+
+    for (let input of midiAccess.inputs.values())
+    {
+        input.onmidimessage = onMidiMessage;
+    }
+}
+
+function onMidiFailure(msg)
+{
+    console.log("Failed to get MIDI access - " + msg);
+}
+
+// If MIDI is supported by this browser
+if ('requestMIDIAccess' in navigator)
+{
+    // Note: no user authorization is required if sysex access is not requested
+    navigator.requestMIDIAccess({ sysex: false }).then(onMidiSuccess, onMidiFailure);
+}
+
+/*
+function getOutPort(midi, devName)
+{
+    var devName = devName.toLowerCase();
+
+    for (var entry of midi.outputs)
+    {
+        var port = entry[1]
+        var name = port.name.toLowerCase();
+
+        if (name.startsWith(devName) || name.endsWith(devName))
+            return port;
+    }
+
+    console.error('could not find output port "' + devName +'"');
+    return null;
+}
+
+function noteOn(noteNo, vel, chanNo)
+{
+    if (vel == undefined)
+        vel = 127;
+
+    if (chanNo == undefined)
+        chanNo = 0;
+
+    // note on, note number, full velocity
+    // omitting the timestamp means send immediately.
+    return [0x90 + chanNo, noteNo, vel];
+}
+
+function noteOff(noteNo, vel, chanNo)
+{
+    if (vel == undefined)
+        vel = 127;
+
+    if (chanNo == undefined)
+        chanNo = 0;
+
+    // note off, note number, full velocity
+    return [0x80 + chanNo, noteNo, vel];
+}
+*/
